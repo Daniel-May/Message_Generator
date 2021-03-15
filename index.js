180b6c8c72a08aeb1833673e1a73bb0841dc4860
Daniel May <danielmay113@gmail.com>
2021-03-15 22:16:47 +0000

    message array added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Javascript file for INDEX.HTML
+
+const message = [
+    'I can do this all day!',
+    'I am Iron Man!',
+    'On your left.',
+    'Bring me THANOS!',
+    'Wakanda Forever!',
+    'We\'re the guardians of the galaxy bitch!'
+]
+
+
