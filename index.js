dadcef8f2acc4d2283d9fed3dff8f5b93a8dfe9b
Daniel May <danielmay113@gmail.com>
2021-03-16 12:11:03 +0000

    bug updates

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const message = [
 
 document.getElementById('generate_button').onclick = function() {
     messageGenerator();
-}
+
+};
 
 
 function randomNumberGen() {
@@ -26,33 +27,41 @@ function messageGenerator() {
     document.getElementById('message').innerHTML = randomMessage;
 
     var superHero = document.getElementById('super');
-
+  
     switch (randomMessage) {
         case 'I can do this all day!':
             superHero.src = 'images/rogers.jpg';
+            superHero.fadeIn(3000);
             break;
 
         case 'I am Iron Man!':
             superHero.src = 'images/stark.jpg';
+            superHero.fadeIn(3000);
             break;
 
         case 'On your left.':
             superHero.src = 'images/rogers.jpg';
+            superHero.fadeIn(3000);
             break;
 
         case 'Bring me THANOS!':
-            superHero.src = 'images/thor.jpg'
+            superHero.src = 'images/thor.jpg';
+            superHero.fadeIn(3000);
             break;
         
         case 'Wakanda Forever!':
             superHero.src = 'images/panther.jpg';
+            superHero.fadeIn(3000);
             break;
 
         case 'We\'re the guardians of the galaxy bitch!':
             superHero.src = 'images/guardians.jpg';
+            superHero.fadeIn(3000);
             break;
 
         default: console.log('Marvel');
-    }
-}
+    };
+
+ 
+};
 
