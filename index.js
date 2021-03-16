e5526ac734277017164cda1cb51aec6a2ea2f326
Daniel May <danielmay113@gmail.com>
2021-03-16 12:14:21 +0000

    switch debugged

--- index.js
+++ index.js
@@ -31,32 +31,26 @@ function messageGenerator() {
     switch (randomMessage) {
         case 'I can do this all day!':
             superHero.src = 'images/rogers.jpg';
-            superHero.fadeIn(3000);
             break;
 
         case 'I am Iron Man!':
             superHero.src = 'images/stark.jpg';
-            superHero.fadeIn(3000);
             break;
 
         case 'On your left.':
             superHero.src = 'images/rogers.jpg';
-            superHero.fadeIn(3000);
             break;
 
         case 'Bring me THANOS!':
             superHero.src = 'images/thor.jpg';
-            superHero.fadeIn(3000);
             break;
         
         case 'Wakanda Forever!':
             superHero.src = 'images/panther.jpg';
-            superHero.fadeIn(3000);
             break;
 
         case 'We\'re the guardians of the galaxy bitch!':
             superHero.src = 'images/guardians.jpg';
-            superHero.fadeIn(3000);
             break;
 
         default: console.log('Marvel');
