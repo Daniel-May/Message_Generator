60e599561d9ad41bf28c710dbc6d0700495fdbd7
Daniel May <danielmay113@gmail.com>
2021-03-16 00:33:40 +0000

    switch added for image phase

--- index.js
+++ index.js
@@ -24,5 +24,33 @@ function randomNumberGen() {
 function messageGenerator() {
     var randomMessage = message[randomNumberGen()];
     document.getElementById('message').innerHTML = randomMessage;
+
+    switch (randomMessage) {
+        case 'I can do this all day!':
+            console.log('Rogers');
+            break;
+
+        case 'I am Iron Man!':
+            console.log('Stark');
+            break;
+
+        case 'On your left.':
+            console.log('Rogers');
+            break;
+
+        case 'Bring me THANOS!':
+            console.log('Thor');
+            break;
+        
+        case 'Wakanda Forever!':
+            console.log('Black Panther');
+            break;
+
+        case 'We\'re the guardians of the galaxy bitch!':
+            console.log('Guardians');
+            break;
+
+        default: console.log('Marvel');
+    }
 }
 
